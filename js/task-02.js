f4b09a5c61c24eed6e35cd62699b21a17bed9285
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-05-25 12:58:07 +0100

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEL = document.querySelector("#ingredients");
+
+const ingredientsList = ingredients.map((ingredient) => {
+  let ingredientEl = document.createElement("li");
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+  
+});
+
+ingredientsEL.append(...ingredientsList);
