8274e1c255ce0203f29a9bd14997e2a5cdb82841
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-07-02 14:15:25 +0100

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColorEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+const bodyColor = document.querySelector("body");
+
+
+btnChangeColorEl.addEventListener("click", changeBodyColor);
+
+function changeBodyColor() {
+  const randomColor = getRandomHexColor();
+  colorNameEl.textContent = randomColor;
+  bodyColor.style.backgroundColor = randomColor;
+}
